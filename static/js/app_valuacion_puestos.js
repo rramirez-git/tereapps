f5d45a7932c738b938448a3e8429c8a6ae38f641
Rubén Ramírez Gómez <maths.unam@gmail.com>
2021-07-06 00:48:59 -0500

    Code updates: restoring and updating inline forms to work as well

--- static/js/app_valuacion_puestos.js
+++ static/js/app_valuacion_puestos.js
@@ -40,3 +40,42 @@ let createGraph = () => {
         });
     }
 }
+
+let AppValuacionPuestosAdjuster = {
+    init() {
+        if(location.href.match(/\/puesto\/\d+\//ig)) {
+            this.generic_read_screen('div_id_puesto');
+        } else if(location.href.match(/\/factor\/\d+\//ig)) {
+            this.generic_read_screen('div_id_factor', 'Factor');
+        } else if(location.href.match(/\/tabulador\/\d+\//ig)) {
+            this.generic_read_screen('div_id_tabulador', 'Tabulador');
+        }
+    },
+    show_main_toolbar() {
+        $("#main-toolbar, #opc-hide-main-toolbar").removeClass('d-none');
+        $("#opc-show-main-toolbar").addClass('d-none');
+    },
+    hide_main_toolbar() {
+        $("#main-toolbar, #opc-hide-main-toolbar").addClass('d-none');
+        $("#opc-show-main-toolbar").removeClass('d-none');
+    },
+    generic_read_screen(id_element_4_np, elemento=null) {
+        this.hide_main_toolbar();
+        $(`#main-toolbar a[title="Listar Todos"]`).remove();
+        let next_prev_div = $(`<div class="col-sm-2 text-left"></div>`);
+        $(`#${id_element_4_np}`).append(next_prev_div);
+        next_prev_div.append($(`a[title="Anterior"]`));
+        next_prev_div.append($(`a[title="Siguiente"]`));
+        for(let opc of [
+            {id: 'opc-show-main-toolbar', fn: 'show_main_toolbar', label: 'Editar' + (elemento ? ' ' + elemento : ''), extraClass: ''},
+            {id: 'opc-hide-main-toolbar', fn: 'hide_main_toolbar', label: 'Cerrar Edicion' + (elemento ? ' de ' + elemento : ''), extraClass: 'd-none'}
+        ]) {
+            $("#cntr-extra-actions").append(
+                `<a id="${opc.id}" class="dropdown-item ${opc.extraClass}" href="#" onclick="AppValuacionPuestosAdjuster.${opc.fn}()">${opc.label}</a>`);
+        }
+    }
+}
+
+$(document).ready(() => {
+    AppValuacionPuestosAdjuster.init();
+})
--- zend_django/hiperforms.py
+++ zend_django/hiperforms.py
@@ -6,6 +6,7 @@ class HorizontalModelForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.helper.form_tag = False
         self.helper.form_class = 'form-horizontal'
         self.helper.label_class = 'col-sm-2 text-right'
         self.helper.field_class = 'col-sm-8'
